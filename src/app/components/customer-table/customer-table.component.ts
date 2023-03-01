b27f4526a9ace8d788a021369ac93f1870272eb1
nicolascalvo73 <nicolascalvo73@gmail.com>
2023-03-01 13:43:46 -0300

    buttons add/remove, checkboxes, sticky header and paginator

--- src/app/components/customer-table/customer-table.component.ts
+++ src/app/components/customer-table/customer-table.component.ts
@@ -1,28 +1,67 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { SelectionModel } from '@angular/cdk/collections';
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort, Sort } from '@angular/material/sort';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Costumers } from 'src/app/data/customers.data';
 import { Costumer } from 'src/app/models/costumer.model';
-import {LiveAnnouncer} from '@angular/cdk/a11y';
-import {MatSortModule} from '@angular/material/sort';
+
 
 
 
 @Component({
   selector: 'app-customer-table',
   standalone: true,
-  imports: [CommonModule, MatPaginatorModule, MatTableModule, MatFormFieldModule, MatInputModule, MatSortModule],
+  imports: [
+    CommonModule, 
+    MatPaginatorModule, 
+    MatTableModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatSortModule, 
+    MatButtonModule,
+    MatCheckboxModule
+  ],
+
   templateUrl: './customer-table.component.html',
   styleUrls: ['./customer-table.component.scss']
 })
 export class CustomerTableComponent { 
 
-  displayedColumns: string[] = ['id', 'name', 'lastName', 'status', 'email', 'phone'];
+  displayedColumns: string[] = ['select', 'id', 'name', 'lastName', 'status', 'email', 'phone'];
   dataSource: MatTableDataSource<Costumer>;
+  selection = new SelectionModel<Costumer>(true, []);
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  toggleAllRows() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: Costumer): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id + 1}`;
+  }
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
@@ -43,7 +82,7 @@ export class CustomerTableComponent {
       this._liveAnnouncer.announce('Sorting cleared');
     }
   }
-
+  
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
